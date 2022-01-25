f35193673eb7fd27cc088b3bd6b96f0da1c025fc
Matthew McArthur <mjmcarthur0917@gmail.com>
2022-01-25 10:46:02 -0600

    Added Strings notes

--- For and while loops.playground/Contents.swift
+++ For and while loops.playground/Contents.swift
@@ -119,3 +119,37 @@ else{
     d+=35;
 }
 print(d);
+
+
+//Strings
+
+var fact = "Swift is a type safe language"
+var dev = "Development of Swift began in 2010"
+var author = "Swift was created by Chris Lattner"
+
+fact.count
+
+fact += ", it has a better memory management"
+
+dev.append(" by Apple")
+
+author.lowercased()
+
+author.uppercased()
+
+author[author.startIndex]
+
+author[author.index(before: author.endIndex)]
+
+dev[dev.startIndex]
+
+dev[dev.index(before: dev.endIndex)]
+
+author[author.index(after: author.startIndex)]
+
+author[author.index(author.startIndex,offsetBy: 5)]
+
+author[author.index(author.endIndex,offsetBy: -5)]
+
+fact[fact.index(fact.endIndex,offsetBy: -4)]
+
