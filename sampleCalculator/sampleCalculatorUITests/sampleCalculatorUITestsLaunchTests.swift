033ea31e859ee9e4b962c9457d2b1c01e7451015
Matthew McArthur <mjmcarthur0917@gmail.com>
2022-01-27 10:36:41 -0600

    Simple Calculator app
    Made a simple calculator to add and display the result

---
+++ sampleCalculator/sampleCalculatorUITests/sampleCalculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sampleCalculatorUITestsLaunchTests.swift
+//  sampleCalculatorUITests
+//
+//  Created by McArthur,Matthew J on 1/27/22.
+//
+
+import XCTest
+
+class sampleCalculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
