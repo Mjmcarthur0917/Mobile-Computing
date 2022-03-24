c5a288fd8544cb0f586ee2986a1d0057b482f033
Matthew McArthur <78877577+Mjmcarthur0917@users.noreply.github.com>
2022-03-24 09:31:54 -0500

    Multiple view app

---
+++ DiscountMultipleViewApp/DiscountMultipleViewApp/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  DiscountMultipleViewApp
+//
+//  Created by Student on 3/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var AmountOutlet: UITextField!
+    
+    @IBOutlet weak var DiscountRateOutlet: UITextField!
+    
+    var priceAfterDiscount = 0.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func CalcDiscountButton(_ sender: UIButton) {
+        var amount = Double(AmountOutlet.text!)
+        var discountRate = Double(DiscountRateOutlet.text!)
+        
+        priceAfterDiscount = amount! - (amount!*discountRate!/100)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var transition = segue.identifier
+        if(transition == "ResultSegue"){
+            //make ResultViewController as a destination
+            var destination = segue.destination as! ResultViewController
+            destination.amount = AmountOutlet.text!
+            destination.discRate = DiscountRateOutlet.text!
+            destination.priceAfterDisc = String(priceAfterDiscount)
+        }
+    }
+    
+}
+
